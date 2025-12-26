f02e8668c324fc9722f45e091c227f8bb4207280
Chris Warren-Smith <chrisws@redsilver>
2025-12-26 20:55:10 +1030

    How far did the Aug 2024 chlr-a spike extend?

--- study/gab/pipeline.sh
+++ study/gab/pipeline.sh
@@ -8,8 +8,8 @@ MARKDOWN_1=../GAB.md
 echo "Updating" && \
     ${VIIRS}/viirs_downloader.sh -t ${VIIRS}/data/viirs_sst && \
     ${VIIRS}/viirs_downloader.sh ${VIIRS}/data/viirs_chlr-a && \
-    (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_sst ${STUDY}/sst.csv) && \
-    (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_chlr-a ${STUDY}/chlr-a.csv) && \
+    (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_sst ${STUDY}/sst.csv --start-date 20180104) && \
+    (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_chlr-a ${STUDY}/chlr-a.csv --start-date 20180104) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Esperance_1' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Esperance_2' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Esperance_3' ${STUDY}/images) && \
@@ -19,7 +19,6 @@ echo "Updating" && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Yalata' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Ceduna' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_map.py "Region map" ${STUDY}/regions.json ${STUDY}/images/map.png) && \
-    (cd ${VIIRS} && python viirs_hab_analysis.py --regions ../study/sa/regions.json ../study/sa/chlr-a.csv --plot ../study/sa/images/hab.png --report ../study/sa/hab.md) && \
     echo '## South Australian algal bloom investigation with VIIRS SST and Chlr-A data' > ${MARKDOWN} && \
     echo '![Regions](./study/sa/images/map.png)' >> ${MARKDOWN} && \
     echo '![Esperance_1](./study/sa/images/viirs_timeseries_Esperance_1.png)' >> ${MARKDOWN} && \
@@ -35,9 +34,7 @@ echo "Updating" && \
     (cd ${VIIRS} && python viirs_yearly_mean.py ${STUDY}/chlr-a.csv >> ${MARKDOWN_1}) && \
     echo '```' >> ${MARKDOWN} && \
     (cd ${VIIRS} && python viirs_coverage.py ${STUDY}/regions.json >> ${MARKDOWN_1}) && \
-    cat NOTES.md >> ${MARKDOWN} && \
     echo >> ${MARKDOWN} && \
     echo '---' >> ${MARKDOWN} && \
-    (cd ${VIIRS} && python viirs_energy_analysis.py --regions .${STUDY}/regions.json ${STUDY}/sst.csv --plot ${STUDY}/images/energy.png --output ${STUDY}/energy.csv) >> ${MARKDOWN} && \
     echo Last updated: `date` >> ${MARKDOWN} && \
     optipng images/*.png
