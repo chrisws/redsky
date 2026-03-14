62fb5527b5d7020ef366c1df791bdbe3d178c9c0
Chris Warren-Smith <chrisws@redsilver>
2026-03-14 16:00:03 +1030

    regenerated for new region boundaries

--- study/sa/pipeline.sh
+++ study/sa/pipeline.sh
@@ -12,9 +12,12 @@ echo "Updating" && \
     (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_sst ${STUDY}/sst.csv) && \
     (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_chlr-a ${STUDY}/chlr-a.csv) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'GAB' ${STUDY}/images) && \
-    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Ceduna' ${STUDY}/images) && \
-    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Port Lincoln' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Ceduna E' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Ceduna W' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Pt Lincoln E ' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Pt Lincoln W' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Spencer Gulf N' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Spencer Gulf C' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Spencer Gulf S' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'SVG - NW' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'SVG - NE' ${STUDY}/images) && \
@@ -27,15 +30,18 @@ echo "Updating" && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Lk Alex' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Victor Harbour-Mt Gambier' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Mt Gambier' ${STUDY}/images) && \
-    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Port Fairy' ${STUDY}/images) && \
+    (cd ${VIIRS} && python viirs_plot.py ${STUDY}/sst.csv ${STUDY}/chlr-a.csv 20180105 'Pt Fairy' ${STUDY}/images) && \
     (cd ${VIIRS} && python viirs_map.py "Region map" ${STUDY}/regions.json ${STUDY}/images/map.png) && \
     (cd ${VIIRS} && python viirs_hab_analysis.py --regions ${STUDY}/regions.json ${STUDY}/chlr-a.csv --plot ${STUDY}/images/hab.png --report ${STUDY}/hab.md) && \
     echo '## South Australian algal bloom investigation with VIIRS SST and Chlr-A data' > ${MARKDOWN} && \
     echo "![Regions](${STUDY_1}/images/map.png)" >> ${MARKDOWN} && \
     echo "![GAB](${STUDY_1}/images/viirs_timeseries_GAB.png)" >> ${MARKDOWN} && \
-    echo "![Ceduna](${STUDY_1}/images/viirs_timeseries_Ceduna.png)" >> ${MARKDOWN} && \
-    echo "![Port Lincoln](${STUDY_1}/images/viirs_timeseries_PortLincoln.png)" >> ${MARKDOWN} && \
+    echo "![Ceduna E](${STUDY_1}/images/viirs_timeseries_CedunaE.png)" >> ${MARKDOWN} && \
+    echo "![Ceduna W](${STUDY_1}/images/viirs_timeseries_CedunaW.png)" >> ${MARKDOWN} && \
+    echo "![Pt Lincoln E](${STUDY_1}/images/viirs_timeseries_PtLincolnE.png)" >> ${MARKDOWN} && \
+    echo "![Pt Lincoln W](${STUDY_1}/images/viirs_timeseries_PtLincolnW.png)" >> ${MARKDOWN} && \
     echo "![Spencer Gulf N](${STUDY_1}/images/viirs_timeseries_SpencerGulfN.png)" >> ${MARKDOWN} && \
+    echo "![Spencer Gulf C](${STUDY_1}/images/viirs_timeseries_SpencerGulfC.png)" >> ${MARKDOWN} && \
     echo "![Spencer Gulf E](${STUDY_1}/images/viirs_timeseries_SpencerGulfS.png)" >> ${MARKDOWN} && \
     echo "![SVG - NW](${STUDY_1}/images/viirs_timeseries_SVGNW.png)" >> ${MARKDOWN} && \
     echo "![SVG - NE](${STUDY_1}/images/viirs_timeseries_SVGNE.png)" >> ${MARKDOWN} && \
@@ -48,7 +54,7 @@ echo "Updating" && \
     echo "![Lk Alex](${STUDY_1}/images/viirs_timeseries_LkAlex.png)" >> ${MARKDOWN} && \
     echo "![Victor Harbour-Mt Gambier](${STUDY_1}/images/viirs_timeseries_VictorHarbourMtGambier.png)" >> ${MARKDOWN} && \
     echo "![Mt Gambier](${STUDY_1}/images/viirs_timeseries_MtGambier.png)" >> ${MARKDOWN} && \
-    echo "![Port Fairy](${STUDY_1}/images/viirs_timeseries_PortFairy.png)" >> ${MARKDOWN} && \
+    echo "![Pt Fairy](${STUDY_1}/images/viirs_timeseries_PtFairy.png)" >> ${MARKDOWN} && \
     (cd ${VIIRS} && python viirs_energy_analysis.py --regions ${STUDY}/regions.json ${STUDY}/sst.csv --plot ${STUDY}/images/energy.png --output ${STUDY}/energy.csv >> ${MARKDOWN_1}) && \
     echo '### Yearly mean values (chlr-a)' >> ${MARKDOWN} && \
     echo '```' >> ${MARKDOWN}&& \
