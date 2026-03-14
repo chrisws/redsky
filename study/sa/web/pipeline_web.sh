8b1af3a9c1ed6df7dec9eaf61e9a2061bf7810cd
Chris Warren-Smith <chrisws@redsilver>
2026-03-15 09:17:24 +1030

    regenerated for new region boundaries

--- study/sa/web/pipeline_web.sh
+++ study/sa/web/pipeline_web.sh
@@ -7,5 +7,5 @@ echo "Updating" && \
     ${VIIRS}/viirs_nrt_downloader.sh 20190101 ${VIIRS}/data/viirs_nrt-chlr-a && \
     (cd ${VIIRS} && python viirs_region_extractor.py ${STUDY}/regions.json ./data/viirs_nrt-chlr-a ${STUDY}/web/chlr-a-nrt.csv) && \
     python csv_to_json_converter.py chlr-a-nrt.csv ../regions.json && \
-    cp chlorophyll_data.js ../../../../chrisws.github.io/ &&\
-    cp bloom-watch.html ../../../../chrisws.github.io/
+    cp chlorophyll_data.js ../../../../chrisws.github.io/bloom-watch
+
